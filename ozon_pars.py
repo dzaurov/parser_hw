8fe624a13db1f266fa4577adb055c68bab72acd2
Egor <63789176+esentemov@users.noreply.github.com>
2020-05-20 17:28:50 +0300

    update_headers

--- ozon_pars.py
+++ ozon_pars.py
@@ -2,6 +2,9 @@ import re
 import requests
 from bs4 import BeautifulSoup
 import os
+from user_agent import generate_user_agent
+
+headers = {'User-Agent': generate_user_agent(device_type="desktop", os=('mac', 'linux'))}
 
 print("Вставьте любую ссылку на категорию Ozon.ru, в которой вы хотите скачать фотографии в высоком качестве,\n"
       "Например, https://www.ozon.ru/highlight/37196/ или https://www.ozon.ru/category/antivirusy-32647/eset-149974849/")
@@ -9,7 +12,7 @@ print("Вставьте любую ссылку на категорию Ozon.ru,
 URL = input("Введите URL: ")
 name_folder = input("Введите название папки: ")
 os.mkdir(name_folder)
-page = requests.get(URL.strip())
+page = requests.get(URL.strip(), timeout=5, headers=headers)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
